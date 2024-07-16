5c5735cd80bf8fa340aa056f0249269deaaab659
Rishabh Shukla <rishabhshukla298@gmail.com>
2024-07-16 13:12:05 +0530

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using MauiAppDemo.Models;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace MauiAppDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            //Navigation.PushAsync(new DemoPage());
+            //count++;
+
+            //if (count == 1)
+            //CounterBtn.Text = $"Clicked {count} time";
+            //else
+            //CounterBtn.Text = $"Clicked {count} times";
+
+            //SemanticScreenReader.Announce(CounterBtn.Text);
+
+            HttpClient client = new HttpClient();
+            string json = client.GetStringAsync("https://dummy.restapiexample.com/api/v1/employees").Result;
+            EmployeeDetailsVM employeeDetails = JsonConvert.DeserializeObject<EmployeeDetailsVM>(json);
+
+        }
+    }
+
+}
